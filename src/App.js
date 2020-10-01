c71ecbe3d28054fc9070d354990ce2ec27934c2e
qodbtn41 <qodbtn41@naver.com>
2020-10-02 01:33:18 +0900

    Update App.js
    setTimeout
    다음시간엔 데이터 페칭이 완료되면 화면을 갱신하는 작업을 하는 코드를 짜볼거에요

--- src/App.js
+++ src/App.js
@@ -1,44 +1,19 @@
 import React from "react";
-import PropTypes from "prop-types";
 
 class App extends React.Component {
   state = {
-    count: 0,
-    test: 10,
+    isLoading: true,
+    movies: [],
   };
-
-  constructor(props) {
-    super(props);
-    console.log("constructor");
-  }
   componentDidMount() {
-    console.log("component did mount");
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 5000);
   }
-  componentDidUpdate() {
-    console.log("component did update");
-  }
-  componentWillUnmount() {
-    console.log("component will unmount");
-  }
-  add = () => {
-    console.log("add");
-    console.log(this.state.test);
-    this.setState((current) => ({ count: current.count + 1 }));
-  };
-  minus = () => {
-    console.log("minus");
-    console.log(this.state.test);
-    this.setState({ count: this.state.count - 1 });
-  };
+
   render() {
-    console.log("component rendering");
-    return (
-      <div>
-        <h1>I am a class component {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
-    );
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading..." : "We are ready"}</div>;
   }
 }
 

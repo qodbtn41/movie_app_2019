1291cbc393662d241acb74a440e5d9ce7ccde41f
qodbtn41 <qodbtn41@naver.com>
2020-10-02 01:27:36 +0900

    Update App.js
    State에 대해서 배웠다.
    State변경시 SetState를 통해서 해라
    setState를 호출하면 알아서 render가 호출되서 화면이 update 된다.
    setState할때는 현재 상태를 가져올때 this.state.어쩌고 이렇게 가져오지 말고 current => 이렇게 해서 가져와야 된다.

--- src/App.js
+++ src/App.js
@@ -1,58 +1,45 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-function Food({ name, image, rating }) {
-  return (
-    <div>
-      <h1>I like {name}</h1>
-      <h4>{rating} / 5.0</h4>
-      <img src={image} />
-    </div>
-  );
-}
+class App extends React.Component {
+  state = {
+    count: 0,
+    test: 10,
+  };
 
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  image: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired,
-};
-const foodlILike = [
-  {
-    key: 1,
-    name: "Potato",
-    image:
-      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcafedelites.com%2Fwp-content%2Fuploads%2F2016%2F06%2FBaked-Potato-Wedges-IMAGE-49.jpg&f=1&nofb=1",
-    rating: 4.9,
-  },
-  {
-    key: 2,
-    name: "ramen",
-    image:
-      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2F1.bp.blogspot.com%2F-R5uQo-tGPJA%2FVGUmdQ46L0I%2FAAAAAAAANTg%2F3GoTSQ8SLWE%2Fs1600%2F515.jpg&f=1&nofb=1",
-    rating: 4.7,
-  },
-  {
-    key: 3,
-    name: "samgiopsal",
-    image:
-      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.gildedgingerbread.com%2Fwp-content%2Fuploads%2F2017%2F08%2FSamgyeopsal-1.jpg&f=1&nofb=1",
-    rating: 8.8,
-  },
-  {
-    key: 4,
-    name: "chukumi",
-    image:
-      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2F3.bp.blogspot.com%2F-z_PjxY9_V7U%2FUW3pnJz6b-I%2FAAAAAAAAANQ%2F8SOJaFB43iw%2Fs1600%2Fchukumi.jpg&f=1&nofb=1",
-    rating: 2.3,
-  },
-];
-
-function renderFood(dish) {
-  return <Food key={dish.key} name={dish.name} image={dish.image} rating={dish.rating} />;
-}
-
-function App() {
-  return <div>{foodlILike.map(renderFood)}</div>;
+  constructor(props) {
+    super(props);
+    console.log("constructor");
+  }
+  componentDidMount() {
+    console.log("component did mount");
+  }
+  componentDidUpdate() {
+    console.log("component did update");
+  }
+  componentWillUnmount() {
+    console.log("component will unmount");
+  }
+  add = () => {
+    console.log("add");
+    console.log(this.state.test);
+    this.setState((current) => ({ count: current.count + 1 }));
+  };
+  minus = () => {
+    console.log("minus");
+    console.log(this.state.test);
+    this.setState({ count: this.state.count - 1 });
+  };
+  render() {
+    console.log("component rendering");
+    return (
+      <div>
+        <h1>I am a class component {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
 }
 
 export default App;

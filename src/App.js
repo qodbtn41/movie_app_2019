5e7f0aaf5d1d0d15b875b1a3eb8126238c92195d
qodbtn41 <qodbtn41@naver.com>
2020-10-02 00:04:12 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from "react";
 
+function Food(props) {
+  console.log(props);
+  return <h1>I like {props.name}</h1>;
+}
 function App() {
   return (
     <div className="App">
       <h1>Hello!!!</h1>
+      <Food name="Potato" />
+      <Food name="ramen" />
+      <Food name="samgiopsal" />
+      <Food name="chukumi" />
+
     </div>
   );
 }

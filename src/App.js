6a7134222cc4e8f0717b99a9e392f60795874cf2
qodbtn41 <qodbtn41@naver.com>
2020-10-03 19:57:20 +0900

    a 태그 스타일 삭제

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import About from "./routes/About";
 import Home from "./routes/Home";
 import Navigation from "./components/Navigation";
 import Detail from "./routes/Detail";
+import "./App.css";
 function App() {
   return (
     <HashRouter>

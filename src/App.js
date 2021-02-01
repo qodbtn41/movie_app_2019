17bd1ff8b0fffa5a9a950ec89c22e5a99238aa01
qodbtn41 <qodbtn41@naver.com>
2021-02-01 23:46:45 +0900

    .

--- src/App.js
+++ src/App.js
@@ -12,16 +12,6 @@ function App() {
       <Route path="/" exact component={Home} />
       <Route path="/about" component={About} />
       <Route path="/movie/:id" component={Detail} />
-
-      {/* <Route path="/home">
-        <h1>Home</h1>
-      </Route>
-      <Route path="/home/introduction">
-        <h1>Introduction</h1>
-      </Route>
-      <Route path="/about">
-        <h1>About</h1>
-      </Route> */}
     </HashRouter>
   );
 }
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -5,7 +5,6 @@ class Detail extends React.Component {
     const { location, history } = this.props;
     if (location.state === undefined) {
       history.push("/");
-      // history.goBack();
     }
   }
   render() {
